61a7ec631e546387849883738ad189235995ae5f
Anton Nemtsev <thesilentimp@gmail.com>
2015-07-16 06:24:38 +0300

    Update 2015-07-16T03:24:33.204Z

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,40 @@ $(document).ready(function() {
   });
 });
 
+var Information,
+  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+Information = (function() {
+  function Information() {
+    this.togglePanel = bind(this.togglePanel, this);
+    $('.information__header').on('click', this.togglePanel);
+  }
+
+  Information.prototype.togglePanel = function(event) {
+    var block, body, link;
+    link = $(event.currentTarget);
+    block = link.parent();
+    body = block.find('.information__body');
+    block.toggleClass('information__section_open');
+    if (block.hasClass('information__section_open')) {
+      return body.velocity("stop").velocity("slideDown", {
+        duration: 250
+      });
+    } else {
+      return body.velocity("stop").velocity("slideUp", {
+        duration: 250
+      });
+    }
+  };
+
+  return Information;
+
+})();
+
+$(document).ready(function() {
+  return new Information;
+});
+
 var Item,
   bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
@@ -29,18 +63,17 @@ Item = (function() {
   }
 
   Item.prototype.size = function(event) {
-    var block;
+    var block, link;
     event.preventDefault();
-    block = $(event.currentTarget.parentNode);
-    this.items.css({
-      width: 295,
-      height: 380
-    });
-    block.css({
-      width: 905,
-      height: 770
-    });
-    return $('.item__list').isotope('layout');
+    link = $(event.currentTarget);
+    block = link.parent();
+    this.items.removeClass('item_open');
+    block.addClass('item_open');
+    return setTimeout((function(_this) {
+      return function() {
+        return $('.item__list').isotope('layout');
+      };
+    })(this), 0);
   };
 
   return Item;
@@ -49,4 +82,22 @@ Item = (function() {
 
 $(document).ready(function() {
   return new Item;
+});
+
+$(document).ready(function() {
+  var element, i, len, ref, results;
+  ref = $('.recommend__list');
+  results = [];
+  for (i = 0, len = ref.length; i < len; i++) {
+    element = ref[i];
+    results.push($(element).slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      variableWidth: true,
+      arrows: true,
+      dots: false
+    }));
+  }
+  return results;
 });
